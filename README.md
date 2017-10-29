5ad644ab3ae2416ea3a2f60c3e76c2c9a0100f30
creativitRy <creativitRy@gmail.com>
2017-10-29 16:54:08 -0500

    lights (sort of)

--- README.md
+++ README.md
@@ -4,4 +4,3 @@ Uses Prim's algorithm to randomly generate maze.
 Libs:
 * [Phaser](http://phaser.io/)
 * [js-priority-queue](https://github.com/adamhooper/js-priority-queue)
-* [jshashtable](https://github.com/timdown/jshashtable)
--- js/mainGame.js
+++ js/mainGame.js
@@ -7,6 +7,9 @@ var fins;
 var input;
 var mazeWidth;
 var mazeHeight;
+var light;
+var opaques;
+var lightMask;
 
 states.mainGame.prototype = {
     preload: function () {
@@ -16,6 +19,7 @@ states.mainGame.prototype = {
         game.load.image('player', 'assets/player.png')
     },
     create: function () {
+        game.plugins.add(Phaser.Plugin.PhaserIlluminated);
         mazeWidth = game.width / 20;
         mazeHeight = game.height / 20;
         
@@ -25,19 +29,23 @@ states.mainGame.prototype = {
         walls = game.add.group();
         walls.enableBody = true;
         
+        fins = game.add.group();
+        fins.enableBody = true;
+        
+        opaques = [];
         createMaze(1, 1);
         
-        player = game.add.sprite(3 * 20 + 1, 4 * 20 + 1, 'player');
+        player = game.add.sprite(1 * 20 + 1, 1 * 20 + 1, 'player');
         game.physics.arcade.enable(player);
         player.body.collideWorldBounds = true;
         player.body.setCircle(9);
         
-        fins = game.add.group();
-        fins.enableBody = true;
-        
-        createFin(14, 14);
-        
         input = game.input.keyboard.createCursorKeys();
+        
+        light = game.add.illuminated.lamp(1 * 20 + 1, 1 * 20 + 1, {distance: 40, });
+        light.createLighting(opaques);
+        lightMask = game.add.illuminated.darkMask([light], 'rgba(0,0,0,0.99)');
+        
     },
     update: function () {
         game.physics.arcade.collide(player, walls);
@@ -55,6 +63,16 @@ states.mainGame.prototype = {
             player.body.velocity.y = -150;
         else if (input.down.isDown)
             player.body.velocity.y = 150;
+        
+        //light
+        light.x = player.x;
+        if (light.x % 20 < 2)
+            light.x += 2;
+        light.y = player.y;
+        if (light.y % 20 < 2)
+            light.y += 2;
+        light.refresh();
+        lightMask.refresh();
     }
 };
 
@@ -89,7 +107,7 @@ function createMaze(startX, startY) {
     markWall(startX, startY);
     while (walls.length) {
         var wall = walls.dequeue();
-        console.log(wall.x + ", " + wall.y);
+        // console.log(wall.x + ", " + wall.y);
         
         if (wallBlocks[coordToIndex(wall.x, wall.y)]) {
             wallBlocks[coordToIndex(wall.xWall, wall.yWall)] = false;
@@ -104,6 +122,15 @@ function createMaze(startX, startY) {
         }
     }
     
+    for (var y = 0; y < mazeHeight; y++) {
+        if (!wallBlocks[coordToIndex(mazeWidth - 1, y)])
+            createFin(mazeWidth - 1, y)
+    }
+    for (var x = 0; x < mazeWidth; x++) {
+        if (!wallBlocks[coordToIndex(x, mazeHeight - 1)])
+            createFin(x, mazeHeight - 1)
+    }
+    
     function markWall(x, y) {
         wallBlocks[coordToIndex(x, y)] = false;
         var tempWalls = getWalls(x, y);
@@ -113,7 +140,7 @@ function createMaze(startX, startY) {
     
     function getWalls(x, y) {
         var walls = [];
-    
+        
         if (inRange(x + 2, y))
             walls.push({x: x + 2, y: y, xWall: x + 1, yWall: y, value: game.rnd.integer()});
         if (inRange(x - 2, y))
@@ -122,7 +149,7 @@ function createMaze(startX, startY) {
             walls.push({x: x, y: y + 2, xWall: x, yWall: y + 1, value: game.rnd.integer()});
         if (inRange(x, y - 2))
             walls.push({x: x, y: y - 2, xWall: x, yWall: y - 1, value: game.rnd.integer()});
-    
+        
         return walls;
     }
     
@@ -138,6 +165,7 @@ function createMaze(startX, startY) {
 function createWall(x, y) {
     var wall = walls.create(x * 20, y * 20, 'block');
     wall.body.immovable = true;
+    opaques.push(game.add.illuminated.rectangleObject(x * 20, y * 20, 20, 20));
 }
 
 function createFin(x, y) {
